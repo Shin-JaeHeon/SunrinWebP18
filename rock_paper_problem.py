9ff14f030fe706d06b8bdcbf5e4f3e641b1db932
Shin-JaeHeon <heoni0504@gmail.com>
2018-03-21 10:04:17 +0900

    Add files via upload

---
+++ rock_paper_problem.py
@@ -0,0 +1,30 @@
+import random;
+
+player = input("(가위, 바위, 보) 중에서 하나를 선택하세요: ");
+
+number = random.randint(0,2);
+(number == 0):
+computer = "가위";
+(number == 1):
+computer = "바위";
+
+computer = "보";
+print("사용자: ", player, "컴퓨터: ", computer)
+
+(player == computer):
+    print("비겼음!");
+player == "바위"
+computer == "보"
+        print("컴퓨터가 이겼음!");
+
+        print("사용자가 이겼음!");
+(player == "보"):
+(computer == "비위"):
+        print("사용자가 이겼음!");
+
+        print("컴퓨터가 이겼음!");
+(player == "가위"):
+(computer == "바위"):
+        print("컴퓨터가 이겼음!");
+
+        print("사용자가 이겼음!");
