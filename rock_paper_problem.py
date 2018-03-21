8d29fc2e9b9ca460020e3be0037c32c597c5dbe6
Shin-JaeHeon <heoni0504@gmail.com>
2018-03-21 10:07:34 +0900

    d

--- rock_paper_problem.py
+++ rock_paper_problem.py
@@ -1,30 +1,30 @@
-import random;
+import random
 
-player = input("(가위, 바위, 보) 중에서 하나를 선택하세요: ");
+player = input("(가위, 바위, 보) 중에서 하나를 선택하세요: ")
 
-number = random.randint(0,2);
-(number == 0):
-computer = "가위";
-(number == 1):
-computer = "바위";
-
-computer = "보";
+number = random.randint(0, 2)
+if number == 0:
+    computer = "가위"
+elif number == 1:
+    computer = "바위"
+else:
+    computer = "보"
 print("사용자: ", player, "컴퓨터: ", computer)
 
-(player == computer):
-    print("비겼음!");
-player == "바위"
-computer == "보"
-        print("컴퓨터가 이겼음!");
-
-        print("사용자가 이겼음!");
-(player == "보"):
-(computer == "비위"):
-        print("사용자가 이겼음!");
-
-        print("컴퓨터가 이겼음!");
-(player == "가위"):
-(computer == "바위"):
-        print("컴퓨터가 이겼음!");
-
-        print("사용자가 이겼음!");
+if player == computer:
+    print("비겼음!")
+if player == "바위":
+    if computer == "보":
+        print("컴퓨터가 이겼음!")
+    else:
+        print("사용자가 이겼음!")
+if player == "보":
+    if computer == "비위":
+        print("사용자가 이겼음!")
+    else:
+        print("컴퓨터가 이겼음!")
+if player == "가위":
+    if computer == "바위":
+        print("컴퓨터가 이겼음!")
+    else:
+        print("사용자가 이겼음!")
